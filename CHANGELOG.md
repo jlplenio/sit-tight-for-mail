00ac95859b7203e4f560468171b4cf7d7636b69e
nubtwo <ubtown@gmail.com>
2016-10-05 05:29:53 +0200

    Working Mydealz, started PriceChecker

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+2016.10.04:  
+Changes:
+- os.path.join, cross platform
+- Interface Order
+- fetch_content fetches multiple Soup objects
+- Dict comprehension to filter out empty searches in json #beproud
+- Started with PriceChecker
--- bot.py
+++ bot.py
@@ -5,15 +5,17 @@ from smtplib import SMTP
 from bs4 import BeautifulSoup
 from requests import get
 
+import os
 import json
 import pymysql
 import time
 
 # better to use while open here?
-json_data=open('config.json').read()
+json_data=open(os.path.join(os.getcwd(), 'config.json')).read()
 json_config = json.loads(json_data)
 
 # ToDo: Close DB connection
+
 # Open database connection
 db = pymysql.connect(host=json_config['sql']['Host'], port=int(json_config['sql']['Port']),
                      user=json_config['sql']['User'], passwd=json_config['sql']['Pass'],
@@ -23,15 +25,40 @@ dbcursor = db.cursor()
 
 class Bot:
 
-
-    def prepare_mail(self, *args):
+    def get_links(self):
         raise NotImplementedError()
 
     def fetch_content(self):
-        content = get(self.get_link())
-        return BeautifulSoup(content.content, "html.parser")
+        """
+        Expects List from self.get_links
+        :return: List of Soup objects
+        """
+
+        links = self.get_links()
+        linklist = []
+        for link in links:
+            content = get(link)
+            print(content)
+            linklist.append(BeautifulSoup(content.content, "html.parser"))
+        return linklist
+
+
+    def process_soup(self, soup):
+        raise NotImplementedError()
+
+    def filter_content(self, processed):
+        raise NotImplementedError()
 
     def select_from_db(self):
+        raise NotImplementedError()
+
+    def insert_in_db(self, datadict):
+        raise NotImplementedError()
+
+    def worth_sending(self, items, search):
+        raise NotImplementedError()
+
+    def prepare_mail(self, *args):
         raise NotImplementedError()
 
     def send_mail(self, to, subject, body):
@@ -47,52 +74,34 @@ class Bot:
         smtp.sendmail(json_config['mail']['From'], to, msg.as_string().encode("utf8"))
         print("Mail versendet")
 
-    def process_soup(self, soup):
-        raise NotImplementedError()
-
-    def filter_content(self, processed):
-        raise NotImplementedError()
-
-    def insert_in_db(self, datadict):
-        raise NotImplementedError()
-
-    def get_link(self):
-        raise NotImplementedError()
-
-    def worth_sending(self, items, search):
-        raise NotImplementedError()
 
 class MydealzBot(Bot):
 
     # Select&Insert-Statements
-    s_latestdeadids = "SELECT `dealid` FROM mydealz ORDER by id desc limit 30"
+    s_latestdealids = "SELECT `dealid` FROM mydealz ORDER by id desc limit 30"
     i_deal = """ INSERT INTO `mydealz`(`id`, `titel`, `stext`, `ltext`, `dlink`, `hlink`, `datum`, `price`, `dealid`)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) """
 
     # better to use while open?
-    json_data = open('mydealzsearches.json').read()
-    json_searches = json.loads(json_data)
-
-    def prepareMail(self, *args):
-        pass
-
-    def h101_preis(self):
-        pass
+    json_data = open(os.path.join(os.getcwd(), 'mydealzsearches.json')).read()
+    json_searches = {i: j for i, j in json.loads(json_data).items() if j != {}} #Dict comprehension BITCH
+    #json_searches = collections.OrderedDict(json_searches)
 
     def select_from_db(self):
-        dbcursor.execute(self.s_latestdeadids)
+        dbcursor.execute(self.s_latestdealids)
         data = dbcursor.fetchall()
         latest = []
         for row in data:
             latest.append(row[0])
         return latest
 
-    def get_link(self):
-        return "http://www.mydealz.de/deals-new"
+    def get_links(self):
+        return ["http://www.mydealz.de/deals-new"]
 
     def process_soup(self, soup):
 
-        deals = soup.find_all('article', class_='thread thread--deal thread--type-list space--mt-2')
+        #soup[0] dirty dirty
+        deals = soup[0].find_all('article', class_='thread thread--deal thread--type-list space--mt-2')
         alldealz = []
 
         for deal in deals:
@@ -117,7 +126,7 @@ class MydealzBot(Bot):
                 'price': price
             })
 
-        return reversed(alldealz)
+        return list(reversed(alldealz))
 
     def filter_content(self, deals):
         latest20 = self.select_from_db()
@@ -160,11 +169,41 @@ class MydealzBot(Bot):
                 None, deal['titel'], deal['stext'], None, deal['dlink'], deal['hlink'],
                 time.strftime('%Y-%m-%d %H:%M:%S'),float(deal['price']),deal['dealid'])) #meh
             i += 1
-        db.commit()
+        #db.commit()
         print(str(i) + " neue Dealz eingetragen")
 
 
 class PriceChecker(Bot):
+
+    """
+    CREATE TABLE `pricechecker` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL , `price`  FLOAT NOT NULL , `date` DATE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+    Weil Bot durch MydealzBot darauf getrimmt ist immer nur eine URL zu durchsuchen und
+    dann nachträglich auf Keywords zu prüfen ist es super schwierig mehrere pricesearches
+    auf unterschiedlichen seiten zu machen ... sehr hässlich bislang, da ich auf die
+    reihenfolge der listen und dicts baue
+
+    Und grrrrr an das Interface halten, wie ne Zwangsjacke
+    """
+
+    # Select&Insert-Statements
+    s_latestprice = """SELECT `price` FROM `pricechecker` WHERE `name` = %s ORDER by `id` desc limit 1"""
+    i_price = """ INSERT INTO `pricechecker`(`id`, `name`, `price`, `date`) VALUES (%s,%s,%s,%s) """
+
+    # better to use while open?
+    json_data = open(os.path.join(os.getcwd(), 'pricesearches.json')).read()
+    json_searches = {i: j for i, j in json.loads(json_data).items() if j != {}} #Dict comprehension BITCH
+
+    def select_from_db(self):
+        dbprices = []
+        for search in self.json_searches:
+                name = self.json_searches[search]['name']
+                dbcursor.execute(self.s_latestprice, name)
+                price = dbcursor.fetchone()
+                price = price[0] if price else None
+                dbprices.append(price)
+        return dbprices
+
     def worth_sending(self, items, search):
         pass
 
@@ -174,35 +213,51 @@ class PriceChecker(Bot):
     def prepare_mail(self, *args):
         pass
 
-    def get_link(self):
-        return "http://www.gearbest.com/rc-quadcopters/pp_230472.html"
+    def get_links(self):
+        linklist = []
+        for search in self.json_searches:
+                linklist.append(self.json_searches[search]['url'])
+        return linklist
 
-    def process_soup(self, soup):
-        price = soup.find_all('span', class_='my_shop_price new_shop_price')[0].contents[0]
-        price = round(float(price) * 0.90998951, 2)
-        return price
+    def process_soup(self, souplist):
+        priceslist = []
+        i = 0
+        #kA, wie ich über souplist iteriere, er cucked mich
+        for search in self.json_searches:
+            price = souplist[i].find(self.json_searches[search]['tag'], class_=self.json_searches[search]['class']).contents[0]
+            priceslist.append(float(price))
+            i += 1
+        return priceslist
 
     def filter_content(self, price):
-        pass
+        print(self.select_from_db())
+        print(price)
+        # Continue
+
 
 def main():
     bot = MydealzBot()
     soup = bot.fetch_content()
-    processed = list(bot.process_soup(soup))
+    processed = (bot.process_soup(soup))
     filtered = bot.filter_content(processed)
     bot.insert_in_db(filtered)
     sendmenow = []
     for search in bot.json_searches:
-        if bot.json_searches[search]:  # if content in subdict, e.g. search3['email']
-            tosendlist = bot.worth_sending(filtered, bot.json_searches[search])
-            for item in tosendlist:
-                subject, body = bot.prepare_mail(*item)
-                sendmenow.append([bot.json_searches[search]['email'], subject, body])
+        tosendlist = bot.worth_sending(filtered, bot.json_searches[search])
+        for item in tosendlist:
+            subject, body = bot.prepare_mail(*item)
+            sendmenow.append([bot.json_searches[search]['email'], subject, body])
     for sendme in sendmenow:
         bot.send_mail(*sendme)
 
 
-main()
+bot = PriceChecker()
+soup = bot.fetch_content()
+processed = bot.process_soup(soup)
+bot.filter_content(processed)
+
+
+
 
 
 
